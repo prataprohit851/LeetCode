bd5183ed46deacf5ba01037a487ea2b06b6a558f
prataprohit851 <88248863+prataprohit851@users.noreply.github.com>
2022-06-28 00:42:33 +0530

    Time: 5241 ms (6.98%), Space: 13.6 MB (51.16%) - LeetHub

---
+++ 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def executeInstructions(self, n, startPos, s):
+        li = []
+        count = 0;
+        ini = startPos[0]
+        inj = startPos[1]
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                if s[j] == 'R' : startPos[1] += 1
+                elif s[j] == 'L' : startPos[1] -= 1
+                elif s[j] == 'U' : startPos[0] -= 1
+                elif s[j] == 'D' : startPos[0] += 1
+                
+                if (startPos[1] > -1 and startPos[1] < n and startPos[0] > -1 and startPos[0] < n) : count += 1
+                else : startPos = [-1,-1] 
+            startPos = [ini, inj]
+            li.append(count)
+            count = 0
+        return li
