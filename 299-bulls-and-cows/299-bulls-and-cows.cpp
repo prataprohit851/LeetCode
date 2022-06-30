cac73d45d41ce56c02e69a420f4666a22ced36f0
prataprohit851 <88248863+prataprohit851@users.noreply.github.com>
2022-06-30 16:39:27 +0530

    Time: 22 ms (6.55%), Space: 6.8 MB (10.54%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.cpp
+++ 299-bulls-and-cows/299-bulls-and-cows.cpp
@@ -4,15 +4,15 @@ public:
         vector<char> ht;
         vector<char> ht2;
         for(int i = 0; i < s.size(); i++) ht.push_back(s[i]);
+        int bullCount = 0;
         for(int i = 0; i < g.size(); i++){
+            if(s[i] == g[i]) bullCount++;
             auto itr = find(ht.begin(), ht.end(), g[i]);
             if(itr != ht.end()) { 
                 ht.erase(itr);
                 ht2.push_back(g[i]);
             }
         }
-        int bullCount = 0;
-        for(int i = 0; i < s.size(); i++) if (s[i] == g[i]) bullCount++;
         int cowCount = ht2.size()-bullCount;
         return to_string(bullCount) + "A" + to_string(cowCount) + "B";
     }
