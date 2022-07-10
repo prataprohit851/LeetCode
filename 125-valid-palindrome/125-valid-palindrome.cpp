dd5a84c1b04fe8ede9df1957cea5747c7c539438
prataprohit851 <88248863+prataprohit851@users.noreply.github.com>
2022-07-10 13:44:37 +0530

    Time: 5 ms (77.93%), Space: 7.3 MB (76.45%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0;
+        int j = s.length()-1;
+        while(i < j){
+            while(i < s.length() && !isalpha(s[i]) && !isdigit(s[i])) i++;
+            while(j >= 0 && !isalpha(s[j]) && !isdigit(s[j])) j--;
+            if(i == s.length() || j < 0) return true;
+            if(islower(s[i])) s[i] = toupper(s[i]);
+            if(islower(s[j])) s[j] = toupper(s[j]);
+            if(s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
