e8c91e8901dd2cc1620cee049d33c2b145cc1a1d
prataprohit851 <88248863+prataprohit851@users.noreply.github.com>
2022-07-17 15:16:53 +0530

    Time: 43 ms (62.18%), Space: 26.1 MB (35.62%) - LeetHub

---
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        vector<int> ans;
+        ans.push_back(first);
+        for(auto ele : encoded)
+            ans.push_back(ans.back()^ele);
+        return ans;
+    }
+};
