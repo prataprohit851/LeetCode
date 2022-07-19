1c019880809511f19309cff6fe138e423c594a6d
prataprohit851 <88248863+prataprohit851@users.noreply.github.com>
2022-07-19 22:44:51 +0530

    Time: 39 ms (5.16%), Space: 18.7 MB (50.09%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
@@ -1,13 +1,33 @@
 class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
-        sort(seats.begin(), seats.end());
-        sort(students.begin(), students.end());
+        priority_queue<int> ss;
+        priority_queue<int> st;
         int n = seats.size();
+        for(int i = 0; i < n; i++){
+            ss.push(seats[i]);
+            st.push(students[i]);
+        }
         int ans = 0;
-        for(int i = 0; i < n; i++)
-            ans += abs(students[i] - seats[i]);
-        
+        while(ss.size() != 0){
+            ans += abs(ss.top() - st.top());
+            ss.pop();
+            st.pop();
+        }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+//         sort(seats.begin(), seats.end());
+//         sort(students.begin(), students.end());
+//         int n = seats.size();
+//         int ans = 0;
+//         for(int i = 0; i < n; i++)
+//             ans += abs(students[i] - seats[i]);
+        
+//         return ans;
+//     }
+// };
