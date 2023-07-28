c68bda8a85fde107ee6f4972b3c48d5f84149d0c
Rohit Kumar <prataprohit851@gmail.com>
2023-07-28 15:53:45 +0530

    Time: 2145 ms (5%), Space: 18.3 MB (99.88%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> ans;
-        sort(intervals.begin(), intervals.end());
-        ans.push_back(intervals[0]);
-        for(int i=1; i<intervals.size(); i++){
-            cout<<i<<" ";
-            if(intervals[i][0] <= ans[ans.size()-1][1]){
-                if(ans[ans.size()-1][1] < intervals[i][1])ans[ans.size()-1][1] = intervals[i][1];
-            }
-            else{
-                ans.push_back(intervals[i]);
+    vector<vector<int>> merge(vector<vector<int>>& interval) {
+        sort(interval.begin(), interval.end());
+        for(int i = 1; i < interval.size(); i++){
+            if(interval[i][0] <= interval[i-1][1]){
+                interval[i-1][1] = max(interval[i][1], interval[i-1][1]);
+                interval.erase(interval.begin()+i);
+                i--;
             }
         }
-        return ans;
+        return interval;
+
     }
+    // static bool cmp(vector<int>& a, vector<int>& b){
+    //     cout<<a[0]<<" "<<b[0]<<endl;
+    //     if(a[0] > b[0]) return false;
+    //     else return a[1] < b[1];
+    // }
 };
