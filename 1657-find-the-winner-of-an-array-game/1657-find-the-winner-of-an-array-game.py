626aa1d7667be90958941ac807cd54f4a3f6c177
Rohit Kumar <prataprohit851@gmail.com>
2023-11-05 18:55:40 +0530

    Time: 541 ms (49.46%), Space: 30 MB (63.04%) - LeetHub

---
+++ 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getWinner(self, arr: List[int], k: int) -> int:
+        curr = arr[0]
+        win = 0
+        for i in range(1, len(arr)):
+            if arr[i] > curr:
+                curr = arr[i]
+                win = 0
+            win += 1
+            if win == k:
+                return curr
+        return curr       
